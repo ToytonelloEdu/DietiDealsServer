287f99803bff16123cb2d79ca932a85c5a2e9bd9
ToytonelloEdu <antonio.ascione15@studenti.unina.it>
2024-07-10 12:06:32 +0200

    DatabaseSession class added

---
+++ DietiDeals_RestAPI/src/main/java/org/example/data/DatabaseSession.java
@@ -0,0 +1,37 @@
+package org.example.data;
+
+import org.example.data.entities.*;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AvailableSettings;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.tool.schema.Action;
+
+public class DatabaseSession {
+    private static final String user = "postgres";
+    private static final String password =
+            "sangio";
+            //"dd_dealsdieti_p455w0rd!24";
+
+    public static final SessionFactory sessionFactory =
+            new Configuration()
+                    .addAnnotatedClass(Auction.class)
+                    .addAnnotatedClass(Bid.class)
+                    .addAnnotatedClass(Tag.class)
+                    .addAnnotatedClass(User.class)
+                    .addAnnotatedClass(Auctioner.class)
+                    .addAnnotatedClass(Buyer.class)
+                    // PostgreSQL
+                    .setProperty(AvailableSettings.JAKARTA_JDBC_URL, "jdbc:postgresql://localhost:5433/dietideals")
+                    // Credentials
+                    .setProperty(AvailableSettings.JAKARTA_JDBC_USER, user)
+                    .setProperty(AvailableSettings.JAKARTA_JDBC_PASSWORD, password)
+                    // Automatic schema export
+                    .setProperty(AvailableSettings.JAKARTA_HBM2DDL_DATABASE_ACTION,
+                            Action.SPEC_ACTION_DROP_AND_CREATE)
+                    // SQL statement logging
+                    .setProperty(AvailableSettings.SHOW_SQL, true)
+                    .setProperty(AvailableSettings.FORMAT_SQL, true)
+                    .setProperty(AvailableSettings.HIGHLIGHT_SQL, true)
+                    // Create a new SessionFactory
+                    .buildSessionFactory();
+}
