65090471f69fb3bb9d9cc8147ead34493170ea02
ToytonelloEdu <antonio.ascione15@studenti.unina.it>
2024-07-16 15:51:55 +0200

    New functions and implementations

--- DietiDeals_RestAPI/src/main/java/org/example/data/repos/UsersDbRepository.java
+++ DietiDeals_RestAPI/src/main/java/org/example/data/repos/UsersDbRepository.java
@@ -1,6 +1,10 @@
 package org.example.data.repos;
 
-import org.example.data.entities.User;
+import org.example.auth.AuthCredentials;
+import org.example.data.entities.*;
+import org.hibernate.Session;
+
+import static org.example.data.DatabaseSession.sessionFactory;
 
 public class UsersDbRepository implements UsersRepository {
     private static UsersDbRepository instance;
@@ -15,7 +19,51 @@ public class UsersDbRepository implements UsersRepository {
     }
 
     @Override
-    public User getUserByID(int id) {
-        return null;
+    public User getUserByUsername(String username) {
+        User subUser = sessionFactory.openSession().find(User.class, username);
+        if(subUser.getUserType().equals("Auctioneer")) {
+            Auctioneer auctioneer = new Auctioneer(subUser);
+            auctioneer.setAuctions(
+                    sessionFactory.openSession()
+                        .createSelectionQuery("FROM Auction WHERE auctioneer = :auctioneer", Auction.class)
+                        .setParameter("auctioneer", auctioneer).getResultList()
+            );
+            return auctioneer;
+        } else {
+            Buyer buyer = new Buyer(subUser);
+            buyer.setBids(
+                    sessionFactory.openSession()
+                        .createSelectionQuery("FROM Bid WHERE buyer = :buyer", Bid.class)
+                        .setParameter("buyer", buyer).getResultList()
+            );
+            return buyer;
+        }
     }
+
+
+    @Override
+    public User addUser(User user) {
+        User subUser;
+        try {
+            if(user.getUserType().equals("Auctioneer")) {
+                subUser = new Auctioneer(user);
+            } else if(user.getUserType().equals("Buyer")) {
+                subUser = new Buyer(user);
+            } else throw new IllegalArgumentException("Invalid user type");
+            sessionFactory.inTransaction(session -> {
+                session.persist(subUser);
+            });
+            return user;
+        } catch (Exception e) {
+            return null;
+        }
+
+    }
+
+    @Override
+    public Boolean verifyCredentials(AuthCredentials auth) {
+        User user = sessionFactory.openSession().find(User.class, auth.getUsername());
+        return user.checkCredentials(auth);
+    }
+
 }
--- DietiDeals_RestAPI/src/main/java/org/example/data/repos/UsersRepository.java
+++ DietiDeals_RestAPI/src/main/java/org/example/data/repos/UsersRepository.java
@@ -1,9 +1,13 @@
 package org.example.data.repos;
 
+import org.example.auth.AuthCredentials;
 import org.example.data.entities.User;
 
 public interface UsersRepository {
 
-    User getUserByID(int id);
+    User getUserByUsername(String username);
 
+    User addUser(User user);
+
+    Boolean verifyCredentials(AuthCredentials auth);
 }
