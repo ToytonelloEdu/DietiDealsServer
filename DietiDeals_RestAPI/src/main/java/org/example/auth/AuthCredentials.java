784472c1e216b43f12ffc410340c4709172039a3
ToytonelloEdu <antonio.ascione15@studenti.unina.it>
2024-07-16 23:35:22 +0200

    Added fields, methods and constructors

--- DietiDeals_RestAPI/src/main/java/org/example/auth/AuthCredentials.java
+++ DietiDeals_RestAPI/src/main/java/org/example/auth/AuthCredentials.java
@@ -3,4 +3,34 @@ package org.example.auth;
 public class AuthCredentials {
     private String username;
     private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public AuthCredentials() {}
+    public AuthCredentials(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "AuthCredentials{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
