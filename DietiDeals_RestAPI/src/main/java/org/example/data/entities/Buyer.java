604b60837f8fe3429b8dc94125e5ef9887900c3e
ToytonelloEdu <antonio.ascione15@studenti.unina.it>
2024-07-16 23:33:59 +0200

    Added fields, methods and changed Sets to Lists

--- DietiDeals_RestAPI/src/main/java/org/example/data/entities/Buyer.java
+++ DietiDeals_RestAPI/src/main/java/org/example/data/entities/Buyer.java
@@ -3,10 +3,29 @@ package org.example.data.entities;
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
 
-import java.util.Set;
+import java.util.List;
 
 @Entity(name = "Buyer")
 public class Buyer extends User{
     @OneToMany(mappedBy= "buyer")
-    private Set<Bid> bids;
+    private List<Bid> bids;
+
+    public List<Bid> getBids() {
+        return bids;
+    }
+
+    public void setBids(List<Bid> bids) {
+        this.bids = bids;
+    }
+
+    public Buyer(){}
+    public Buyer(User user) {
+        setUsername(user.getUsername());
+        setPassword(user.getPassword());
+        setEmail(user.getEmail());
+        setFirstName(user.getFirstName());
+        setLastName(user.getLastName());
+        setBio(user.getBio());
+        setNationality(user.getNationality());
+    }
 }
