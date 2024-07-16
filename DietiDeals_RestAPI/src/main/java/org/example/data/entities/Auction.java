0ee8bb770564761eb7757f126dbcfaf2bdb61301
ToytonelloEdu <antonio.ascione15@studenti.unina.it>
2024-07-16 23:33:46 +0200

    Added fields, methods and changed Sets to Lists

--- DietiDeals_RestAPI/src/main/java/org/example/data/entities/Auction.java
+++ DietiDeals_RestAPI/src/main/java/org/example/data/entities/Auction.java
@@ -3,33 +3,53 @@ package org.example.data.entities;
 import jakarta.persistence.*;
 
 import java.sql.Timestamp;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 
 import static jakarta.persistence.FetchType.LAZY;
 
 @Entity(name = "Auction")
 public class Auction {
 
+    //TODO: CREATE AUCTION SUBCLASSES
     @Id @GeneratedValue
     private int id;
 
+
     private String picturePath;
+
     private String objectName;
     private String description;
 
-    @ManyToOne(fetch = LAZY)
-    private Auctioner auctioner;
+    @ManyToOne
+    private Auctioneer auctioneer;
 
     private Timestamp date;
 
     @ManyToMany
     @JoinTable(name = "auctionTags")
-    private Set<Tag> tags;
+    private List<Tag> tags;
 
-    @OneToMany(mappedBy= "auction")
-    private Set<Bid> bids;
+    @OneToMany(mappedBy= "auction", fetch = LAZY)
+    private List<Bid> bids;
+
+    @Transient
+    private Bid lastBid = getLastBid();
+    @Transient
+    private String auctioneerUsername;
 
     public Auction() {}
+    public Auction(int id) {
+        this.id = id;
+    }
+    public Auction(int id, String picturePath, String objectName, String description, Timestamp date) {
+        this.id = id;
+        this.picturePath = picturePath;
+        this.objectName = objectName;
+        this.description = description;
+        this.date = date;
+    }
+
 
     public int getId() {
         return id;
@@ -63,12 +83,12 @@ public class Auction {
         this.description = description;
     }
 
-    public Auctioner getAuctioner() {
-        return auctioner;
+    public Auctioneer getAuctioneer() {
+        return auctioneer;
     }
 
-    public void setAuctioner(Auctioner auctioner) {
-        this.auctioner = auctioner;
+    public void setAuctioneer(Auctioneer auctioneer) {
+        this.auctioneer = auctioneer;
     }
 
     public Timestamp getDate() {
@@ -79,19 +99,47 @@ public class Auction {
         this.date = date;
     }
 
-    public Set<Tag> getTags() {
+    public List<Tag> getTags() {
         return tags;
     }
 
-    public void setTags(Set<Tag> tags) {
+    public void setTags(List<Tag> tags) {
         this.tags = tags;
     }
 
-    public Set<Bid> getBids() {
+    public List<Bid> getBids() {
         return bids;
     }
 
-    public void setBids(Set<Bid> bids) {
+    public void setBids(List<Bid> bids) {
         this.bids = bids;
+        this.lastBid = getLastBid();
+    }
+
+    public String getAuctioneerUsername() {
+        return auctioneerUsername;
+    }
+
+    public void setAuctioneerUsername(String auctioneerUsername) {
+        this.auctioneerUsername = auctioneerUsername;
+    }
+
+    public Bid getLastBid() {
+        if (bids != null && !bids.isEmpty()) {
+            int size = bids.size();
+            return bids.get(size-1);
+        } else return null;
+    }
+
+    public boolean addBid(Bid bid) {
+        if(bids == null) {
+            bids = new ArrayList<>();
+        }
+        if(!bids.contains(bid) && bid.getAmount() > lastBid.getAmount())
+        {
+            boolean res = bids.add(bid);
+            lastBid = getLastBid();
+            return true;
+        } else return false;
     }
 }
