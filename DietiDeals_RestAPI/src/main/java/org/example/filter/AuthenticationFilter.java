c7ead2db285163b872c4679c64d78beef7f78c76
ToytonelloEdu <antonio.ascione15@studenti.unina.it>
2024-07-26 20:07:39 +0200

    Added RequireAuth @interface and Filter class that provides it (for route-protection)

---
+++ DietiDeals_RestAPI/src/main/java/org/example/filter/AuthenticationFilter.java
@@ -0,0 +1,19 @@
+package org.example.filter;
+
+import jakarta.annotation.Priority;
+import jakarta.ws.rs.Priorities;
+import jakarta.ws.rs.container.ContainerRequestContext;
+import jakarta.ws.rs.container.ContainerRequestFilter;
+import jakarta.ws.rs.ext.Provider;
+
+import java.io.IOException;
+
+@Provider
+@RequireAuth
+@Priority(Priorities.AUTHENTICATION)
+public class AuthenticationFilter implements ContainerRequestFilter {
+    @Override
+    public void filter(ContainerRequestContext containerRequestContext) throws IOException {
+
+    }
+}
