9d4766ea33c0cb75ff746883bc4c43d01df47643
ToytonelloEdu <antonio.ascione15@studenti.unina.it>
2024-08-08 14:21:18 +0200

    Implemented filter method

--- DietiDeals_RestAPI/src/main/java/org/example/filter/AuthenticationFilter.java
+++ DietiDeals_RestAPI/src/main/java/org/example/filter/AuthenticationFilter.java
@@ -1,10 +1,14 @@
 package org.example.filter;
 
 import jakarta.annotation.Priority;
+import jakarta.ws.rs.NotAuthorizedException;
 import jakarta.ws.rs.Priorities;
 import jakarta.ws.rs.container.ContainerRequestContext;
 import jakarta.ws.rs.container.ContainerRequestFilter;
+import jakarta.ws.rs.core.HttpHeaders;
+import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.Provider;
+import org.example.auth.JwtAuthController;
 
 import java.io.IOException;
 
@@ -14,6 +18,25 @@ import java.io.IOException;
 public class AuthenticationFilter implements ContainerRequestFilter {
     @Override
     public void filter(ContainerRequestContext containerRequestContext) throws IOException {
-        //TODO: Implement
+        System.out.println("Filtering request");
+        // Get the HTTP Authorization header from the request
+        String authorizationHeader = containerRequestContext.getHeaderString(HttpHeaders.AUTHORIZATION);
+
+        System.out.println("Authorization header: " + authorizationHeader);
+
+        // Check if the HTTP Authorization header is present and formatted correctly
+        if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
+            System.out.println("Invalid authorizationHeader : " + authorizationHeader);
+            throw new NotAuthorizedException("Authorization header must be provided");
+        }
+
+        String token = authorizationHeader.substring("Bearer".length()).trim();
+
+        if( JwtAuthController.getInstance().validateToken(token) ){
+            System.out.println("Token is valid: " + token);
+        } else {
+            System.out.println("Token is NOT valid: " + token);
+            containerRequestContext.abortWith(Response.status(Response.Status.UNAUTHORIZED).build());
+        }
     }
 }
