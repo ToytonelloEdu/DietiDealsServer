17293b03e4db0d96a4a8900a206cce74bf7b02f2
ToytonelloEdu <antonio.ascione15@studenti.unina.it>
2024-09-14 00:47:21 +0200

    Created resource for Notifications endpoints

---
+++ DietiDeals_RestAPI/src/main/java/org/example/NotificationResource.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.WebApplicationException;
+import jakarta.ws.rs.core.Response;
+import org.example.data.entities.Notification;
+import org.example.data.entities.User;
+import org.example.data.repos.NotificationsDbRepository;
+import org.example.data.repos.NotificationsRepository;
+import org.example.data.repos.UsersDbRepository;
+import org.example.data.repos.UsersRepository;
+
+import java.util.List;
+
+@Path("notifications")
+public class NotificationResource {
+    private final NotificationsRepository notificationsRepo;
+
+    public NotificationResource() {
+        notificationsRepo = NotificationsDbRepository.getInstance();
+    }
+
+    @GET
+    @Path("{handle}")
+    public List<Notification> getNotifications(@PathParam("handle") String handle) {
+        try{
+            return notificationsRepo.getNotificationsByUser(handle);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            throw new WebApplicationException(Response.Status.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+}
