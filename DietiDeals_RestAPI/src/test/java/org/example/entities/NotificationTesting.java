31d1d4bb77560e95f1fa870ef3464337050b0b21
ToytonelloEdu <antonio.ascione15@studenti.unina.it>
2024-09-26 11:45:41 +0200

    Create NotificationTesting.java

---
+++ DietiDeals_RestAPI/src/test/java/org/example/entities/NotificationTesting.java
@@ -0,0 +1,15 @@
+package org.example.entities;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class NotificationTesting {
+
+    @Test
+    public void testNotification() {
+
+    }
+}
