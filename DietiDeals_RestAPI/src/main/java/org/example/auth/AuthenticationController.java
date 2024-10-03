0894e3f81c7992effc459fe5788fe3d000afe1c4
ToytonelloEdu <antonio.ascione15@studenti.unina.it>
2024-10-03 10:35:07 +0200

    Removed Cognito

--- DietiDeals_RestAPI/src/main/java/org/example/auth/AuthenticationController.java
+++
@@ -1,4 +0,0 @@
-package org.example.auth;
-
-public interface AuthenticationController {
-}
--- DietiDeals_RestAPI/src/main/java/org/example/auth/cognito/CognitoAuthenticationController.java
+++
@@ -1,7 +0,0 @@
-package org.example.auth.cognito;
-
-import org.example.auth.AuthenticationController;
-
-public class CognitoAuthenticationController implements AuthenticationController {
-
-}
